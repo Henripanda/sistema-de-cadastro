5cc2ffa3e211d9a485a5a2b4237b0296bcc59a22
Clemente Marques <clemente.marques@matriz.libercard.com.br>
2022-11-02 11:02:52 -0300

    restore 2

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
 console.log("Rodando o Sistema de Gerenciamento de Jogos")
 console.log("Aplicação esta conectando ao Banco de Dados")
 console.log("Teste de alteração no vscode")
-console.log("enviando para o github")
